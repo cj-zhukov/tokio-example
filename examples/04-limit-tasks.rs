b9ecc9733aa553eaf33b990abccc8067f62964a1
Sergey Zhukov <szhukov@aligntech.com>
2025-01-26 09:15:29 +0300

    docs: add new example 04-limit-task

--- examples/04-limit-tasks.rs
+++ examples/04-limit-tasks.rs
@@ -10,45 +10,41 @@ const MAX_CONCURRENT_TASKS: usize = 2;
 async fn main() -> Result<()> {
     let ids = ["1", "x", "3", "4", "5", "6", "7", "8", "9", "a"];
     let mut tasks = JoinSet::new();
-    let mut outputs = vec![];
     
     for id in ids {
-        while tasks.len() >= MAX_CONCURRENT_TASKS {
+        tasks.spawn(foo(id));
+
+        if tasks.len() == MAX_CONCURRENT_TASKS {
             if let Some (res) = tasks.join_next().await {
                 match res {
                     Ok(res) => match res {
-                        Ok(val) => outputs.push(val),
-                        Err(e) => println!("failed processing data: {}", e)
+                        Ok(_) => (),
+                        Err(e) => println!("failed running foo: {}", e)
                     },
-                    Err(e) => println!("failed running foo: {}", e)
+                    Err(e) => println!("failed running task: {}", e)
                 }
             }
         }
-        tasks.spawn(foo(id));
     }
-    
-    println!("done spawning");
-    
+
     while let Some(res) = tasks.join_next().await {
         match res {
             Ok(res) => match res {
-                Ok(val) => outputs.push(val),
-                Err(e) => println!("failed processing data: {}", e)
+                Ok(_) => (),
+                Err(e) => println!("failed running foo: {}", e)
             },
-            Err(e) => println!("failed running foo: {}", e)
+            Err(e) => println!("failed running task: {}", e)
         }
     }
-    
-    println!("all done");
-    println!("{:?}", outputs);
 
     Ok(())
 }
 
-async fn foo(x: &str) -> Result<i32> {
+async fn foo(x: &str) -> Result<()> {
     println!("start processing input: {}", x);
     let num: u64 = thread_rng().gen_range(0..=10);
     sleep(Duration::from_secs(num)).await;
-
-    Ok(x.parse::<i32>()?)
+    let res = x.parse::<i32>()?;
+    println!("{}", res);
+    Ok(())
 }
